385a47b4a3e4045e591755f7af32d8a8b456ec2f
maried2 <marie.duran@snowflake.com>
2024-11-11 19:00:08 -0500

    Update cortex_search_create.sql
    Co-authored-by: Daniel Silva <daniel.silva@snowflake.com>

--- cortex_search_create.sql
+++ cortex_search_create.sql
@@ -1,18 +1,10 @@
 USE DATABASE cortex_analyst_demo;
 USE SCHEMA revenue_timeseries;
 
-CREATE OR REPLACE DYNAMIC TABLE product_landing_table
-  WAREHOUSE = cortex_analyst_wh
-  TARGET_LAG = '1 hour'
-  AS (
-      SELECT DISTINCT product_line AS product_dimension FROM product_dim
-  );
-
-
   CREATE OR REPLACE CORTEX SEARCH SERVICE product_line_search_service
   ON product_dimension
   WAREHOUSE = cortex_analyst_wh
   TARGET_LAG = '1 hour'
   AS (
-      SELECT product_dimension FROM product_landing_table
+      SELECT DISTINCT product_line AS product_dimension FROM product_dim
   );
